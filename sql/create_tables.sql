94429b481a3f0326b60c5ce3fed1aed4158fbb4e
FrederiqueK <frederiquekeijzer@gmail.com>
2020-12-11 11:39:08 +0000

    2

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,26 +1,28 @@
 DROP TABLE IF EXISTS products; 
 CREATE TABLE products (
   ID SERIAL PRIMARY KEY,
-  code INTEGER(15),
-  name INTEGER(255),
+  code TEXT,
+  name TEXT,
   description TEXT,
-  price INTEGER(10, 2)
+  price INTEGER,
   kleur_id INTEGER,
   materiaal_id INTEGER,
-  -- attribuut_id INTEGER,
   maat_id INTEGER
 );
 
+DROP TABLE IF EXISTS kleuren; 
 CREATE TABLE kleuren (
   ID SERIAL PRIMARY KEY,
   kleur TEXT
 );
 
+DROP TABLE IF EXISTS materialen; 
 CREATE TABLE materialen (
   ID SERIAL PRIMARY KEY,
   materiaal TEXT
 );
 
+DROP TABLE IF EXISTS maten;
 CREATE TABLE maten (
   ID SERIAL PRIMARY KEY,
   maat TEXT
